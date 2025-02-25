1949fb0e7394d7ba473eba7a72980ba29225c420
Taha Zafar <muhammadtahazafar6@gmail.com>
2025-02-25 22:23:52 +0500

    Create continue-and-break-statement.py

---
+++ continue-and-break-statement.py
@@ -0,0 +1,14 @@
+print("You entered the 5 number")#BREAK STATEMENT
+for i in range(1,20):
+  if(i==12):
+     break
+  print(i)
+print("loop end ")
+
+print("You entered the 5 number")#CONTINU  STATEMENT
+for i in range(1,20):
+   if(i==12):
+     continue
+   print(i)
+print("loop end ")
+
