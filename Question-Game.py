8c337cc456fe77c304db0a2711103f0bdb06532c
Taha Zafar <muhammadtahazafar6@gmail.com>
2025-02-25 23:05:03 +0500

    Create Question-Game.py

---
+++ Question-Game.py
@@ -0,0 +1,10 @@
+a=int(input("tell me does bird fly or not,\n (1)yes,\n(2)NO,\n(3),maybe\n"))
+match a:
+   case 1:
+      print("correct answer you win 10M")
+   case 2:
+       print("you lose")  
+   case 3:
+       print("you lose,\n")  
+
+list1 = ["tell me the bisrrd fly or not answer is yes "]
